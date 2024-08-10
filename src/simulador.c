6236303bee827cb07cd2ba3a9543dc79625bdf2a
Carla B. Ferreira <carlabeatrizferreira04@gmail.com>
2024-08-10 20:26:56 -0300

    Update simulador.c
    Inclusão da leitura dos 3 argumentos corretamente

--- src/simulador.c
+++ src/simulador.c
@@ -1,32 +1,49 @@
 //---------------------------------------------------------------------
 // Arquivo	: simulador.c
 // Conteúdo	: Implementação do TP3 de OC1 2024
-// Autor		: Carla Beatriz Ferreira, Gabriele Pinheiro Sá e
+// Autor	: Carla Beatriz Ferreira, Gabriele Pinheiro Sá e
 //			  Manuela Monteiro Fernandes de Oliveira
-// Histórico	: 2024-08-08 - arquivo criado
+// Histórico: 2024-08-08 - arquivo criado
+//            2024-08-10 - Implementada a leitura dos argumentos corretamente      
 // ---------------------------------------------------------------------
 
 #include <stdio.h>
-#include <sys/types.h>
+#include <sys/types.h> // ou #include <stdint.h> para uint32_t
+#include <stdint.h>
+
+typedef struct EspacoNaCache{
+    uint32_t EspacoEnderecamento[10]; //32 bits
+    //todo
+} EspacoNaCache;
+
 
 int main (int argc, char *argv[]){
-    if(argc == 4){ // Verifica se a quantidade de argumentos está correta
-        unsigned int TamanhoTotalCache;
-        unsigned int TamanhoCadaLinha;
-        unsigned int TamanhoCadaGrupo;
-        int EspacoEnderecamento = 32; //32 bits
-        //todo
+    if(argc != 5){ // Verifica se a quantidade de argumentos está correta
+        printf("Erro, parametros incorretos!\n"
+                "Uso: TamanhoTotalCache TamanhoCadaLinha TamanhoCadaGrupo NomeArquivoComAcessosAMemoria\n");
+        return 1;
+    }
 
-    } else printf("Erro, parametros incorretos!\n"
-                  "Uso: TamanhoTotalCache TamanhoCadaLinha TamanhoCadaGrupo NomeArquivoComAcessosAMemoria\n");
+    FILE *file_input = fopen(argv[4], "r");
+
+    if (file_input == NULL) { // Verifica se o arquivo foi aberto com sucesso
+        perror("Erro ao abrir o arquivo");
+        return 1;
+    }
+
+    unsigned int TamanhoTotalCache;
+    unsigned int TamanhoCadaLinha;
+    unsigned int TamanhoCadaGrupo;
+    char *endptr;
+
+    // Converte o argumento (argv[i]) para unsigned long, e então para unsigned int, 10 = base decimal
+    TamanhoTotalCache = (unsigned int)strtoul(argv[1], &endptr, 10);
+    TamanhoCadaLinha = (unsigned int)strtoul(argv[2], &endptr, 10);
+    TamanhoCadaGrupo = (unsigned int)strtoul(argv[3], &endptr, 10);
+
+    //todo
+
+    // Fechar o arquivo após o uso
+    fclose(file_input);
     return 0;
 }
-
-/*
-    POSSIVEIS FUNÇÕES A SEREM IMPLEMENTADAS
-    1) Exibir a saisa corretamente no arquivo output.txt com fprintf
-        prinft("================\n"
-                "IDX V ** ADDR **\n");
-
-    2) 
-*/
